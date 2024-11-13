d407b35f2e17658572861ef79306a1b6294e504d
jQueryScript <iqq800@gmail.com>
2024-11-13 22:57:31 +0800

    Update generate-readme.js

--- generate-readme.js
+++ generate-readme.js
@@ -83,7 +83,7 @@ for (const [category, features] of Object.entries(css)) {
 }
 
 // Generate README content
-let readmeContent = '# New CSS Features Since 2018\n\n';
+let readmeContent = '# New CSS Features Since 2016\n\n';
 readmeContent += 'A comprehensive list of new CSS features by year of general availability across major browsers.\n\n';
 readmeContent += '_This document is automatically generated weekly._\n\n';
 
